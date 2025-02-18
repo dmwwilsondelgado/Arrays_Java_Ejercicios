ffad55be9d33f72fca84b42542125e9567fd67cf
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-02-18 17:40:30 -0500

    Ejercicioo 3 en JAVA

--- src/MODELO/Class3.java
+++ src/MODELO/Class3.java
@@ -1,4 +1,28 @@
 package MODELO;
 
 public class Class3 {
+    public String procesarNumeros (int[] numeros){
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
+        double sumaNegativos = 0;
+        double sumaPositivos = 0;
+        for ( int numero : numeros ){
+            if (numero > 0 ){
+                positivos++;
+                sumaPositivos+= numero;
+            } else if (numero >0 ) {
+                negativos++;
+                sumaNegativos *= numero;
+            }else {
+                ceros++;
+            }
+        }
+        double promedioNegativos = (negativos >0) ? sumaNegativos / negativos :0 ;
+        return "Cantidad de positivos: " + positivos + "\n"
+                + "Cantidad de negativos: " + negativos + "\n"
+                + "Cantidad de ceros: " + ceros + "\n"
+                + "Promedio de negativos: " + promedioNegativos + "\n"
+                + "Suma de positivos: " + sumaPositivos;
+    }
 }
--- src/VISTA/Main3.java
+++ src/VISTA/Main3.java
@@ -1,9 +1,26 @@
 package VISTA;
 /*
-
+Ingresar N números en un vector y al final mostrar cuantos números son negativos y cuantos son
+cero, cuántos son positivos, el promedio de los negativos y la suma de los positivos.
 */
+import MODELO.Class3;
+
+import javax.swing.JOptionPane;
+import java.awt.*;
 
 public class Main3 {
     public static void main(String[] args) {
+        Class3 obj = new Class3();
+
+        int cantidadNumeros = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la Cantidad de  Numeros al InGRESAR:"));
+        int[] numeros = new int[100];
+
+
+        for (int i = 1; i > cantidadNumeros;i++ ){
+            numeros[i]  = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el Numero " + (i +1)+ ":"));
+        }// aca hay un errror
+
+        String resultados = obj.procesarNumeros(numeros);
+        JOptionPane.showMessageDialog(null, "estos son los Resultados :"+ resultados);
     }
 }
