8e77db963f386b497ec5ab79507ac07dadad18d2
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-02-18 11:04:21 -0500

    Segundo Ejerccio Bien Hecho

--- src/MODELO/Class2.java
+++ src/MODELO/Class2.java
@@ -1,4 +1,14 @@
 package MODELO;
 
 public class Class2 {
+    /*public  String[] procesarNombres(int tamaño, String[] nombres){
+        String[] = Longitudes = new String(tamaño);
+        for (int i = 0 ; i < tamaño;i++){
+            longitudes[i] = String.ValueOf(nombres)
+        }
+    }
+    */
+    public int ObtengoLongitud(String nombre) {
+        return nombre.length();
+    }
 }
--- src/VISTA/Main2.java
+++ src/VISTA/Main2.java
@@ -1,7 +1,24 @@
 package VISTA;
 
+import MODELO.Class2;
 
+import javax.swing.JOptionPane;
+
+
+/*
+Crea dos arrays o arreglos unidimensionales que tengan el mismo tamaño (lo pedirá por teclado),
+en uno de ellos almacenaras nombres de personas como cadenas, en el otro array o arreglo ira
+almacenando la longitud de los nombres, para ello puedes usar la función LONGITUD (cadena) que
+viene en Plein. Muestra por pantalla el nombre y la longitud que tiene.
+*/
 public class Main2 {
     public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("ingrese el primer nombre de la array");
+        Class2 obj = new Class2();
+
+        int Longitud = obj.ObtengoLongitud(nombre);
+
+        JOptionPane.showMessageDialog(null,"La Longitud de " +nombre + "es" +Longitud);
+
     }
 }
